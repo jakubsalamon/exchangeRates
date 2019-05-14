d890e71b1a221cee7cf813ed2cd6913675228cb5
Jakub Salamon <jakubsalamon@o2.pl>
2019-05-14 20:54:19 +0200

    end of 14may session

--- src/components/calculator.jsx
+++ src/components/calculator.jsx
@@ -3,14 +3,14 @@ import '../css/calc.css';
 
 class Calculator extends Component {
     state = { 
-        yourMoney: 0
+        yourMoney: ""
      }
 
      handleInputChange(e) {
-         console.log("ty ciulu");
-         console.log(e.target.value);
+         console.log(e.target);
+         console.log(e);
          this.setState({
-             yourMoney: this.e.target.value
+             yourMoney: e.target.value
          })
      }
 
@@ -19,7 +19,7 @@ class Calculator extends Component {
             <>
             <div className = "calc">
 
-                <input onChange = {this.handleInputChange} type = "text"></input>
+                <input onChange = {this.handleInputChange.bind(this)} type = "text"></input>
                 <button>Wprowadź</button>
                 <p className = "result">Ile za to kupisz: {this.state.yourMoney}</p>
 
